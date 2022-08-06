291843bb43e7cc0ef44b1d09b669a1c981dceb2f
wl <1686201650@qq.com>
2022-08-06 20:09:05 +0800

    短信缓存

--- src/main/java/cn/wl/reggie/contorller/UserController.java
+++ src/main/java/cn/wl/reggie/contorller/UserController.java
@@ -9,6 +9,7 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 
@@ -24,7 +26,8 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private UserService userService;
-
+    @Autowired
+    private RedisTemplate redisTemplate;
     @PostMapping("/sendMsg")
     public R<String> sendMsg(@RequestBody User user, HttpSession session) {
         //获取手机号
@@ -38,7 +41,8 @@ public class UserController {
             //调用阿里云提供的短息服务A皮完成发生短信
            // SMSUtils.sendMessage("瑞吉外卖"," ",phone,code);
             //需要将生成的验证码保存到Session
-            session.setAttribute(phone, code);
+            //session.setAttribute(phone, code);
+            redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
             return R.success("手机验证码短信发送成功");
         }
         return R.error("短信发送失败");
@@ -56,9 +60,15 @@ public class UserController {
        log.info("打印"+map.toString());
 
 
+
+
+
         String phone = map.get("phone").toString();
         String code = map.get("code").toString();
-        Object codeSession = session.getAttribute(phone);
+
+       // Object codeSession = session.getAttribute(phone);
+
+        Object  codeSession  = redisTemplate.opsForValue().get(phone);
         if(codeSession!=null&&codeSession.equals(code)){
             LambdaQueryWrapper<User> queryWrapper=new LambdaQueryWrapper<>();
             queryWrapper.eq(User::getPhone,phone);
@@ -70,6 +80,8 @@ public class UserController {
                 userService.save(user);
             }
             session.setAttribute("user",user.getId());
+            //如果用户登陆成功
+            redisTemplate.delete(phone);
             return  R.success(user);
         }
         return R.error("短信发送失败");
